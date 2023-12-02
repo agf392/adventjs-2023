18db443f29c71fe481a81326d0de4b44d8234979
Antonio Gamazo Ferrero <agf392@gmail.com>
2023-12-02 02:29:42 +0100

    day1 challenge

---
+++ challenges/day1.js
@@ -0,0 +1,9 @@
+function findFirstRepeated(gifts) {
+    let copyArray = [];
+    for (let i = 0; i < gifts.length; i++) {
+      if (copyArray.includes(gifts[i])) return gifts[i];
+      copyArray.push(gifts[i]);
+    }
+    return -1;
+  }
+  
